1bd541f39220907cfe88daf0aada919f6c9652ee
chandit7 <rksmart1611@gmail.com>
2022-05-27 16:32:38 +0530

    Changes done in pom.xml to migrate Swaggerfox to springDoc

--- src/main/java/com/ramesh/server/main/user/UserResource.java
+++ src/main/java/com/ramesh/server/main/user/UserResource.java
@@ -1,8 +1,6 @@
 package com.ramesh.server.main.user;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,21 +9,20 @@ import java.util.List;
 public class UserResource {
     @Autowired
     private UserDaoService service;
+
     @GetMapping("/users")
-    public List<User> retrieveAllusers(){
+    public List<User> retrieveAllusers() {
         return service.findAll();
     }
 
     @GetMapping("/user/{id}")
-    public User retriveUser(@PathVariable int id)
-    {
+    public User retriveUser(@PathVariable int id) {
         return service.findOne(id);
     }
-
     //Add a new user
     @PostMapping("/users")
-    public void addUser(@RequestBody User user){
-       User savedUser = service.save(user);
+    public void addUser(@RequestBody User user) {
+        User savedUser = service.save(user);
 
     }
 
